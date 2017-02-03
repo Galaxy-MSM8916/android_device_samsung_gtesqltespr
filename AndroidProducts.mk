5f16d108ab44d31607de10eb5a4d7db9562ee14a
deadman96385 <seanhoyt963@gmail.com>
2017-02-02 22:51:24 -0600

    Remove omni and cm makefiles
    AndroidProducts isn't needed

--- AndroidProducts.mk
+++
@@ -1,4 +0,0 @@
-LOCAL_PATH := device/samsung/gtesqltespr
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cm.mk \
-    $(LOCAL_DIR)/lineage_gtesqltespr.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
 for i in eng userdebug user; do
-	add_lunch_combo cm_gtesqltespr-${i}
 	add_lunch_combo lineage_gtesqltespr-${i}
 done
