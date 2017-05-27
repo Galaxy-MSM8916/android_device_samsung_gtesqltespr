9b6090f02281a7e9afcadd1c490a433a67cddb4b
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-27 14:21:27 -0400

    gte: Move media config files to msm8916-common

--- device.mk
+++ device.mk
@@ -15,8 +15,3 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/gtesqltespr/overlay
 
 # RIL Shim
 PRODUCT_PACKAGES += libril_shim
-
-# Media configurations
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
