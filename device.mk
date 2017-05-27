b0e2fbdc948a4f25ca220b7debec8241d8e1b0a7
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-27 14:20:12 -0400

    gte: Use stock mixer paths

--- device.mk
+++ device.mk
@@ -2,7 +2,7 @@
 $(call inherit-product-if-exists, vendor/samsung/gtesqltespr/gtesqltespr-vendor.mk)
 
 # Inherit from common
-USE_QCOM_MIXER_PATHS := 0
+USE_QCOM_MIXER_PATHS := 1
 $(call inherit-product, device/samsung/gte-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/gtesqltespr
@@ -21,8 +21,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio/acdb/Handset_cal.acdb:system/etc/Handset_cal.acdb \
 	$(LOCAL_PATH)/configs/audio/acdb/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
 	$(LOCAL_PATH)/configs/audio/acdb/Headset_cal.acdb:system/etc/Headset_cal.acdb \
-	$(LOCAL_PATH)/configs/audio/acdb/Speaker_cal.acdb:system/etc/Speaker_cal.acdb \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+	$(LOCAL_PATH)/configs/audio/acdb/Speaker_cal.acdb:system/etc/Speaker_cal.acdb
 
 # RIL Shim
 PRODUCT_PACKAGES += libril_shim
