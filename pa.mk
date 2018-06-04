97002fd036e1f945078a5ca606e325b9cee3d6fe
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-06-04 01:51:15 -0400

    gtesqltespr: Add PA Makefile
    Change-Id: I02222419e4d3a95cc1439a6bf7da5199c4df36fe

---
+++ pa.mk
@@ -0,0 +1,19 @@
+
+# Inherit from common
+$(call inherit-product, device/samsung/gte-common/pa.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device.mk
+$(call inherit-product, device/samsung/gtesqltespr/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gtesqltespr
+PRODUCT_NAME := pa_gtesqltespr
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T377P
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
