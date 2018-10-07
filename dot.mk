9804b3657e1a09075e6c2467801bbdeef124fc66
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-10-07 01:12:59 -0400

    gtesqltespr: Add dotOS Makefile
    Change-Id: I281213255cfb8baad047242bc0754308b707b847

---
+++ dot.mk
@@ -0,0 +1,22 @@
+
+# Inherit from common
+$(call inherit-product, device/samsung/gte-common/dot.mk)
+
+# Inherit some common Dot OS stuff.
+$(call inherit-product, vendor/dot/config/telephony.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device.mk
+$(call inherit-product, device/samsung/gtesqltespr/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gtesqltespr
+PRODUCT_NAME := dot_gtesqltespr
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T377P
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
